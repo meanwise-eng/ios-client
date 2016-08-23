97174f83b4602e7eba941c2070cdfec518ffd06f
Vishal Kumar <vishalkumar7458@gmail.com>
2016-08-23 13:39:10 +0530

    Initial Commit

---
+++ Meanwise/Meanwise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meanwise
+//
+//  Created by Vishal on 23/08/16.
+//  Copyright © 2016 Squelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
