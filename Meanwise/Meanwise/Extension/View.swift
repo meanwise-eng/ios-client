4f76ab011f7325acba3050f3a4e8ecefaa3e07d9
Vishal Kumar <vishalkumar7458@gmail.com>
2016-09-13 21:33:09 +0530

    added uiview extension

---
+++ Meanwise/Meanwise/Extension/View.swift
@@ -0,0 +1,36 @@
+//
+//  View.swift
+//  Meanwise
+//
+//  Created by Vishal on 13/09/16.
+//  Copyright © 2016 Squelo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: View
+
+extension UIView {
+    
+    // Get height
+    func getHeight() -> CGFloat {
+        return frame.size.height
+    }
+    
+    // Get height
+    func getWidth() -> CGFloat {
+        return frame.size.width
+    }
+    
+    // Get height
+    func getX() -> CGFloat {
+        return frame.origin.x
+    }
+    
+    // Get height
+    func getY() -> CGFloat {
+        return frame.origin.y
+    }
+    
+}
