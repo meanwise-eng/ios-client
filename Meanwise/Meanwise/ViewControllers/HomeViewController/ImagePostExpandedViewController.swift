aecef7dfe7033b624e0035af39f57c96a5eb20dd
Vishal Kumar <vishalkumar7458@gmail.com>
2016-09-14 03:14:18 +0530

    feature: added edge pan gesture for dismissal

--- Meanwise/Meanwise/ViewControllers/HomeViewController/ImagePostExpandedViewController.swift
+++ Meanwise/Meanwise/ViewControllers/HomeViewController/ImagePostExpandedViewController.swift
@@ -17,31 +17,56 @@ class ImagePostExpandedViewController: BaseViewController {
     // MARK: - Variables
     
     var backgroundImageName: String?
-
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        uiSetup()
+        panGestureSetup()
+    }
+    
+    // MARK: - Functions
+    
+    func topBarSetup() {
         navigationController?.navigationBarHidden = true
         UIApplication.sharedApplication().statusBarHidden = true
+    }
+    
+    func uiSetup() {
+        topBarSetup()
         self.backgroundImageView.image = UIImage(named: backgroundImageName!)
     }
-
+    
     // MARK: - Memory Management
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - Deinit
+    
+    deinit {
+        print("ImagePostExpandedViewController DeInitialising")
+    }
+    
 }
 
 extension ImagePostExpandedViewController {
     
-    @IBAction func swipedDown(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: nil)
+    func panGestureSetup() {
+        let edgeGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(ImagePostExpandedViewController.userSwipedFromEdge(_:)))
+        edgeGestureRecognizer.edges = UIRectEdge.Top
+        self.view.addGestureRecognizer(edgeGestureRecognizer)
+    }
+    
+    func userSwipedFromEdge(sender: UIScreenEdgePanGestureRecognizer) {
+        if sender.edges == UIRectEdge.Top {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
     }
     
 }
