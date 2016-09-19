b8f77a3c16d26e49808b17e87e788dcbc8eef8c8
Vishal Kumar <vishalkumar7458@gmail.com>
2016-09-19 18:05:54 +0530

    feature: created choose channel view controller

---
+++ Meanwise/Meanwise/ViewControllers/HomeViewController/ChooseChannelViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChooseChannelViewController.swift
+//  Meanwise
+//
+//  Created by Vishal on 19/09/16.
+//  Copyright © 2016 Squelo. All rights reserved.
+//
+
+import UIKit
+
+class ChooseChannelViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
